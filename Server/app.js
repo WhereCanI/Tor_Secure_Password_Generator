2942d88c030c446c207c30dfde56d0460eeee194
Michael Andersen <michael_andersen93@pm.me>
2021-07-13 10:26:07 +0100

    Initial

---
+++ Server/app.js
@@ -0,0 +1,28 @@
+const Koa = require('koa');
+const helmet = require('koa-helmet');
+const serve = require('koa-static');
+const path = require('path');
+const render = require('koa-ejs');
+const app = new Koa();
+const KoaRouter = require('koa-router');
+const router = new KoaRouter();
+
+app.proxy = true;
+app.use(helmet());
+app.use(serve('.'));
+
+// RENDERS HTML PAGES
+render(app, {
+    root: path.join(__dirname, '../Website'),
+    layout: false,
+    viewExt: 'html',
+    cache: false,
+    debug: false,
+});
+
+// ROUTES
+router.get("/", async ctx => {
+    await ctx.render('passwordGenerator');
+});
+
+app.listen(7005, () => console.log("Password Generator server - Running."));
